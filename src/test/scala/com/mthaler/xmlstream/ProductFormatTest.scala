552721da60116af3dee29c14fd2470bde6b33342
Michael Thaler <michael.thaler@dlr.de>
2015-12-16 10:16:13 +0100

    Renamed ProductFormatTest2 to ProductFormatTest.

--- src/test/scala/com/mthaler/xmlstream/ProductFormatTest.scala
+++ src/test/scala/com/mthaler/xmlstream/ProductFormatTest.scala
@@ -1,8 +1,8 @@
 package com.mthaler.xmlstream
 
+import org.scalatest.FunSuite
+
 import ProductFormat._
-import org.junit.Assert._
-import org.junit.Test
 import scala.reflect.classTag
 
 object ProductFormatTest {
@@ -13,69 +13,64 @@ object ProductFormatTest {
   case class Product1WithOption(option: Option[Int])
 }
 
-class ProductFormatTest {
+class ProductFormatTest extends FunSuite {
 
   import ProductFormatTest._
 
-  @Test
-  def testExtractFieldNames(): Unit = {
+  test("extractFieldNames") {
     val fieldNames = ProductFormat.extractFieldNames(classTag[TestClass])
-    assertEquals(Seq("field1", "field2", "field3"), fieldNames.toSeq)
+    assert(Seq("field1", "field2", "field3") == fieldNames.toSeq)
   }
 
-  @Test
-  def testXmlFormat1(): Unit = {
+  test("xmlFormat1") {
     import BasicAttrFormats._
     implicit val f = xmlFormat1(Product1)
     val p = Product1("test")
     val result0 = f.write(p)
-    assertEquals(Left(<Product1 field1="test"/>), result0)
+    assert(Left(<Product1 field1="test"/>) == result0)
     val result1 = f.read(result0)
-    assertEquals(p, result1)
+    assert(p == result1)
     val f2 = xmlFormat1(Product1WithProduct1)
     val p2 = Product1WithProduct1(Product1("test"))
     val result2 = f2.write(p2)
-    assertEquals(Left(<Product1WithProduct1><product1 field1="test"/></Product1WithProduct1>), result2)
+    assert(Left(<Product1WithProduct1><product1 field1="test"/></Product1WithProduct1>) == result2)
     val result3 = f2.read(result2)
-    assertEquals(p2, result3)
+    assert(p2 == result3)
   }
 
-  @Test
-  def testXmlFormat2(): Unit = {
+  test("xmlFormat2") {
     import com.mthaler.xmlstream.BasicAttrFormats._
     val f = xmlFormat2(Product2)
     val p = Product2("test", 42)
     val result0 = f.write(p)
-    assertEquals(Left(<Product2 field1="test" field2="42"/>), result0)
+    assert(Left(<Product2 field1="test" field2="42"/>) == result0)
     val result1 = f.read(result0)
-    assertEquals(p, result1)
+    assert(p == result1)
   }
 
-  @Test
-  def testXmlFormat1WithOption(): Unit = {
+  test("xmlFormat1WithOptions") {
     import com.mthaler.xmlstream.BasicAttrFormats._
     import com.mthaler.xmlstream.StandardFormats._
     implicit val f = xmlFormat1(Product1WithOption)
     val p = Product1WithOption(Some(42))
     val result0 = f.write(p)
-    assertEquals(Left(<Product1WithOption option="42"/>), result0)
+    assert(Left(<Product1WithOption option="42"/>) == result0)
     val result1 = f.read(result0)
-    assertEquals(p, result1)
+    assert(p == result1)
     val p2 = Product1WithOption(None)
     val result2 = f.write(p2)
-    assertEquals(Left(<Product1WithOption/>), result2)
+    assert(Left(<Product1WithOption/>) == result2)
     val result3 = f.read(result2)
-    assertEquals(p2, result3)
+    assert(p2 == result3)
   }
 
-  @Test
-  def testXmlFormat2CustomFieldNames() {
+  test("xmlFormat2CustomFieldNames") {
     import com.mthaler.xmlstream.BasicAttrFormats._
     val f = xmlFormat(Product2.apply, "myfield1", "myfield2")
     val p = Product2("test", 42)
     val result0 = f.write(p)
-    assertEquals(Left(<Product2 myfield1="test" myfield2="42"/>), result0)
+    assert(Left(<Product2 myfield1="test" myfield2="42"/>) == result0)
     val result1 = f.read(result0)
-    assertEquals(p, result1)
+    assert(p == result1)
   }
 }
