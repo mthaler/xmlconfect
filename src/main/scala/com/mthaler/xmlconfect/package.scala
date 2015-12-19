bf641a59f6b9d81870c2d486efc30fd71d74662b
Michael Thaler <michael.thaler@dlr.de>
2015-12-19 06:48:18 +0100

    Added implicit method that adds convertTo method to XML elem.

--- src/main/scala/com/mthaler/xmlconfect/package.scala
+++ src/main/scala/com/mthaler/xmlconfect/package.scala
@@ -1,6 +1,7 @@
 package com.mthaler
 
 import scala.xml.{ Text, Attribute, MetaData, Node, Null, Elem, TopScope }
+import scala.language.implicitConversions
 
 package object xmlconfect {
 
@@ -14,10 +15,17 @@ package object xmlconfect {
 
   def xmlReader[T](implicit reader: XmlReader[T]) = reader
   def xmlWriter[T](implicit writer: XmlWriter[T]) = writer
+
+  implicit def pimpElem(elem: Elem) = new PimpedElem(elem)
 }
 
 package xmlconfect {
   case class DeserializationException(msg: String, cause: Throwable = null, fieldNames: List[String] = Nil) extends RuntimeException(msg, cause)
 
   class SerializationException(msg: String) extends RuntimeException(msg)
+
+  private[xmlconfect] class PimpedElem(val elem: Elem) extends AnyVal {
+
+    def convertTo[T](implicit reader: XmlElemReader[T]): T = reader.read(Left(elem))
+  }
 }
