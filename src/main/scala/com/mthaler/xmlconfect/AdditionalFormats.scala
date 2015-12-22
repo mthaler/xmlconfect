f668d5425cf5af129997dff57972f81ba49d4b4d
Michael Thaler <michael.thaler@dlr.de>
2015-12-22 17:03:38 +0100

    Added another namedFormat that takes a transformation.

--- src/main/scala/com/mthaler/xmlconfect/AdditionalFormats.scala
+++ src/main/scala/com/mthaler/xmlconfect/AdditionalFormats.scala
@@ -27,7 +27,9 @@ object AdditionalFormats {
    * Constructs a XmlFormat from its two parts, JsonReader and JsonWriter.
    */
   def xmlFormat[T](reader: XmlReader[T], writer: XmlWriter[T]) = new XmlFormat[T] {
+
     def write(obj: T, name: String = "") = writer.write(obj, name)
+
     def read(xml: XML, name: String = "") = reader.read(xml, name)
   }
 
@@ -35,7 +37,9 @@ object AdditionalFormats {
    * Constructs a XmlAttrFormat from its two parts, XmlAttrReader and XmlAttrWriter.
    */
   def xmlAttrFormat[T](reader: XmlAttrReader[T], writer: XmlAttrWriter[T]) = new XmlAttrFormat[T] {
+
     protected override def writeAttr(obj: T, name: String = ""): MetaData = writer.write(obj, name).right.get
+
     protected def readAttr(metaData: MetaData, name: String = ""): T = reader.read(Right(metaData), name)
   }
 
@@ -43,12 +47,24 @@ object AdditionalFormats {
    * Constructs a XmlElemFormat from its two parts, XmlElemReader and XmlElemWriter.
    */
   def xmlElemFormat[T](reader: XmlElemReader[T], writer: XmlElemWriter[T]) = new XmlElemFormat[T] {
+
     protected override def writeElem0(obj: T, name: String = "") = writer.write(obj, name).left.get
+
     protected def readElem(tnode: TNode, name: String = ""): T = reader.read(Left(tnode), name)
   }
 
-  def namedFormat[T](format: XmlFormat[T], name: String) = new XmlFormat[T] {
-    override def read(xml: XML, n: String): T = format.read(xml, name)
-    override def write(obj: T, n: String): XML = format.write(obj, name)
+  def namedFormat[T](format: XmlElemFormat[T], name: String) = new XmlElemFormat[T] {
+
+    override protected def readElem(tnode: TNode, n: String): T = format.read(Left(tnode), name)
+
+    override protected def writeElem0(obj: T, n: String): TNode = format.write(obj, name).left.get
   }
+
+  def namedFormat[T](format: XmlElemFormat[T], name: String, transform: Node => Node) = new XmlElemFormat[T] {
+
+    override protected def readElem(tnode: TNode, n: String): T = format.read(Left(TNode(tnode.node, transform)), name)
+
+    override protected def writeElem0(obj: T, n: String): TNode = format.write(obj, name).left.get
+  }
+
 }
