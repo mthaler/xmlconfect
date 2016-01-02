54abb7e53f07e9f0e186636c3575004aced6cee1
Michael Thaler <michael.thaler@dlr.de>
2016-01-02 11:09:22 +0100

    Added additional namedFormat test.

--- src/test/scala/com/mthaler/xmlconfect/AdditionalFormatsTest.scala
+++ src/test/scala/com/mthaler/xmlconfect/AdditionalFormatsTest.scala
@@ -1,5 +1,6 @@
 package com.mthaler.xmlconfect
 
+import com.mthaler.xmlconfect
 import org.scalatest.FunSuite
 import ProductFormat._
 
@@ -8,6 +9,7 @@ import scala.xml._
 object AdditionalFormatsTest {
   case class Friend(name: String)
   case class Person(name: String, friends: List[Friend])
+  case class Integer(value: Int)
 }
 
 class AdditionalFormatsTest extends FunSuite {
@@ -89,6 +91,27 @@ class AdditionalFormatsTest extends FunSuite {
   }
 
   test("namedFormat") {
+    import BasicAttrFormats._
+    import ProductFormat._
+    implicit val intFormat = xmlFormat1(Integer)
+    implicit val intListFomat = CollectionFormats.listFormat[Integer]
+    assertResult(<Integers><Integer value="5"/><Integer value="10"/><Integer value="15"/></Integers>) {
+      List(Integer(5), Integer(10), Integer(15)).toNode("Integers")
+    }
+    assertResult(List(Integer(5), Integer(10), Integer(15))) {
+      <Integers><Integer value="5"/><Integer value="10"/><Integer value="15"/></Integers>.convertTo[List[Integer]]
+    }
+    val namedIntFormat = AdditionalFormats.namedFormat(xmlFormat1(Integer), "Int")
+    val namedIntListFomat = CollectionFormats.listFormat[Integer](namedIntFormat)
+    assertResult(<Integers><Int value="5"/><Int value="10"/><Int value="15"/></Integers>) {
+      List(Integer(5), Integer(10), Integer(15)).toNode("Integers")(namedIntListFomat)
+    }
+    assertResult(List(Integer(5), Integer(10), Integer(15))) {
+      <Integers><Int value="5"/><Int value="10"/><Int value="15"/></Integers>.convertTo[List[Integer]](namedIntListFomat)
+    }
+  }
+
+  test("namedFormat2") {
     import BasicAttrFormats._
     import ProductFormat._
     implicit val friendFormat = xmlFormat1(Friend)
