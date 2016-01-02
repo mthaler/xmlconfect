3ac1d6227db8f231e8ca001109039c82ef574a91
Michael Thaler <michael.thaler@dlr.de>
2016-01-02 10:41:21 +0100

    Removed lazyFormat which did not work correctly.

--- src/main/scala/com/mthaler/xmlconfect/AdditionalFormats.scala
+++ src/main/scala/com/mthaler/xmlconfect/AdditionalFormats.scala
@@ -7,15 +7,6 @@ import ProductFormat._
 object AdditionalFormats {
 
   /**
-   * Lazy wrapper around serialization. Useful when you want to serialize (mutually) recursive structures.
-   */
-  def lazyFormat[T](format: => XmlFormat[T]) = new XmlFormat[T] {
-    lazy val delegate = format
-    def write(x: T, name: String = "") = delegate.write(x, name)
-    def read(value: XML, name: String = "") = delegate.read(value, name)
-  }
-
-  /**
    * Creates a special format that ignores a value
    */
   def ignoreFormat[T](value: T) = new XmlFormat[T] {
